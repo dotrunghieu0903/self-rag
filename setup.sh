0d141b0e99ec6daad8029baefd56582e7930094c
HieuDo <dotrunghieu0903@gmail.com>
2025-04-20 12:47:26 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-!pip install -r requirements.txt
-!pip install flash-attn==2.3.6
-!pip install faiss-gpu
+pip install -r requirements.txt
+pip install flash-attn==2.3.6
+pip install faiss-gpu
